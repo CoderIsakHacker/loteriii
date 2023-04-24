8fd5e34ceb2373ef228d3ee2932b1a881f48e3e0
Isak Nyström <isny0617@storsjogymnasiet.se>
2023-04-24 14:42:48 +0200

    dinmor

---
+++ lotteri.py
@@ -0,0 +1,11 @@
+import random
+
+class lotteri;
+
+def __init_(self):
+
+    self.list_vinster = ["solstol", "iphone", "chips", "cola me dipp", "bra låtar att lyysssna på ",]
+    
+def get_vinst(self):
+    return self.list_vinster()
+
---
+++ m.py
@@ -0,0 +1,15 @@
+import lotteri
+import os
+
+lotteri = lotteri.lotteri()
+
+print(lotteri.get_vinst)
+
+while looping == True:
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    play_again = input("\n\n\do you want to play again? y/h")
+    
+    if (play_again == "h")
+        break
+,§1
